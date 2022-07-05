4c77fc9ed2981ae4b47936505b985540caf83009
Andrey Larin <lestex@gmail.com>
2022-07-05 17:17:14 -0400

    fix runtime validation

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -12,10 +12,10 @@ import (
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	// Use the new readJSON() helper to decode the request body into the input struct.
